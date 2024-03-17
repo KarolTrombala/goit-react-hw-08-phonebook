7922f7b485fb679bbf4ccb6ef783a7c17444a993
KarolTrombala <116751735+KarolTrombala@users.noreply.github.com>
2024-03-17 22:03:54 +0000

    Deploying to gh-pages from @ KarolTrombala/goit-react-hw-08-phonebook@67b959b002babe601c459063ea9cef5ca5183ab2 🚀

---
+++ static/js/936.300574da.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[936],{4936:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r="Home_container__w+u63",c="Home_title__L087v",a=(n(2791),n(3329)),o=function(){return(0,a.jsx)("div",{className:r,children:(0,a.jsx)("h1",{className:c,children:(0,a.jsx)("p",{children:"My Phonebook"})})})}}}]);
+//# sourceMappingURL=936.300574da.chunk.js.map
