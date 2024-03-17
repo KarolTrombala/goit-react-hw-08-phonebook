67b959b002babe601c459063ea9cef5ca5183ab2
Karol Trombala <ktrombala@gmail.com>
2024-03-17 23:03:07 +0100

    e

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,11 @@
 import css from './Home.module.css';
 import React from 'react';
-import { Hero } from 'components/Hero/Hero';
 
 const Home = () => (
     <div className={css.container}>
         <h1 className={css.title}>
             <p>My Phonebook</p>
         </h1>
-        <Hero />
     </div>
 );
 
